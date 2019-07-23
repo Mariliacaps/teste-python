a49f365553b5e3363fdd2e7290b9d6cd11f5f4b0
mariliacaps <mari.capistrano@hotmail.com>
2019-07-23 13:44:16 -0300

    commit

---
+++ CAP05/EXERCICO05-14.py
@@ -0,0 +1,11 @@
+s=0
+digitados=0
+while True:
+    numero=int(input("Digite um número inteiro a somar e depois digite 0 para sair: "))
+    if numero==0:
+        break;
+    s+=numero
+    digitados=digitados+1
+print("Quantidade de números digitados: %d " %digitados)
+print(f"Soma: {s}")
+print("Média: %6.2f" %(s/digitados))
